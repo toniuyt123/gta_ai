a3e4848ee17a4122fd7dfed00ed6bc5f08c2d8d7
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-05-19 22:30:52 +0300

    added converter to hot representation

---
+++ convert_hot_array.py
@@ -0,0 +1,37 @@
+import numpy as np
+from balancing_data import get_indexes
+file_name = 'data/training_data.npy'
+save_name = 'data/training_hot_data.npy'
+train_data = np.load(file_name)
+
+def convert(file_name, train_data):
+    result = []
+    for data in train_data:
+        image = data[0]
+        choice = data[1]
+        indexes = get_indexes(choice, 1)
+        if  len(indexes) > 2:
+            continue
+        #                A  W  S  D WA WD SA SD 
+        result_choice = [0, 0, 0, 0, 0, 0, 0, 0]
+        if indexes[0] == 0 and indexes[1] == 1:
+            result_choice[4] = 1
+        elif indexes[0] == 1 and indexes[1] == 3:
+            result_choice[5] = 1
+        elif indexes[0] == 0 and indexes[1] == 2:
+            result_choice[6] = 1
+        elif indexes[0] == 2 and indexes[1] == 3:
+            result_choice[7] = 1
+        elif len(indexes) < 2:
+            result_choice[indexes[0]] = 1
+            
+        result.append([image, result_choice])
+    
+    np.save(file_name, result)
+
+
+def main():
+    convert(save_name, train_data)
+
+
+main()
