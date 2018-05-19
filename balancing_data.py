cef613cc82eff98d7dfd09cc5910cd664f1a6a41
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-05-19 03:29:03 +0300

    Refactored some scripts and added a folder to git ignore

---
+++ balancing_data.py
@@ -0,0 +1,59 @@
+import numpy as np
+import random
+import cv2
+
+file_name = 'training_data.npy'
+train_data = np.load(file_name)
+random.shuffle(train_data)
+result = []
+
+counters = []
+
+def get_indexes(val_list, key):
+    return [i for i, j in enumerate(val_list) if j == key]
+
+def process_counters(counters, train_data):
+    for data in train_data:
+        choice = data[1]
+        #print_screen(data[0])
+
+        indexes = get_indexes(choice, 1)
+        for index in indexes:
+            counters[index] += 1
+
+    print(counters)
+    shortest = min(counters)
+    counters = [0] * len(counters)
+    return shortest
+
+def balance_data(file_name, train_data):
+    global counters, result
+    counters = [0] * train_data[0][1]
+    shortest = process_counters(counters, train_data)
+    for data in train_data:
+        image = data[0]
+        choice = data[1]
+
+        indexes = get_indexes(choice, 1)
+        for index in indexes:
+            if counters[index] < shortest:
+                counters[index] += 1
+                result.append([image, choice])
+
+    random.shuffle(result)
+
+    np.save(file_name, result)
+
+def main():
+    global file_name, train_data
+    balance_data(file_name, train_data)
+
+def print_screen(screen):
+    screen = cv2.resize(screen, (800, 600))
+    cv2.imshow('image', screen)
+    #print(train_data[0][1])
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows() 
+        break
+
+main()
--- data_creation.py
+++ data_creation.py
@@ -8,6 +8,7 @@ import win32api as wapi
 import time
 
 stopped = True
+fileName = 'training_data.npy'
 
 keyList = ["\b"]
 for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'APS$/\\":
@@ -21,50 +22,55 @@ def key_check():
     return keys
 
 def key_to_output(keys) :
-    output = [0,0,0]
+    output = [0,0,0,0]
     global stopped
     if 'A' in keys:
         output[0] = 1
-    elif 'D' in keys:
-        output[2] = 1
-    else:
+    if 'D' in keys:
+        output[3] = 1
+    if 'W' in keys:
         output[1] = 1
+    if 'S' in keys:
+        output[2] = 1
 
     if 'P' in keys:
         stopped = not stopped
-        print("Stopped" if stopped else "Resumed")
+        print('Stopped' if stopped else 'Resumed')
         time.sleep(1)
 
     return output
-
-fileName = 'training_data.npy'
-
-if os.path.isfile(fileName):
-    print('File already exists')
-    trainingData = list(np.load(fileName))
-else:
-    print('Creating new file')
+def create_file(fileName):
     trainingData = []
+    if os.path.isfile(fileName):
+        print('File already exists')
+        trainingData = list(np.load(fileName))
+    else:
+        print('Creating new file')
+    return trainingData
 
-time.sleep(5)
-while(True): 
-    key = key_to_output(key_check())
-    if not stopped:
-        prevTime = time.time()
-        screen =  np.array(ImageGrab.grab(bbox=(50,50,800,650)))
-        screen = cv2.cvtColor(screen, cv2.COLOR_RGB2GRAY)
-        screen = cv2.resize(screen, (120, 90))
-        #cv2.imshow('window',screen)
-        print(key)
-        trainingData.append([screen, key])
-        if len(trainingData) % 1000 == 0:
-            print(len(trainingData))
-            np.save(fileName,trainingData)
+def main(fileName):
+    trainingData = create_file(fileName)
+    time.sleep(5)
+    print('Stopped press p to resume')
+    while(True): 
+        key = key_to_output(key_check())
+        if not stopped:
+            prevTime = time.time()
+            screen =  np.array(ImageGrab.grab(bbox=(50,50,800,650)))
+            screen = cv2.cvtColor(screen, cv2.COLOR_RGB2GRAY)
+            screen = cv2.resize(screen, (120, 90))
+            #cv2.imshow('window',screen)
+            print(key)
+            trainingData.append([screen, key])
+            if len(trainingData) % 1000 == 0:
+                print(len(trainingData))
+                np.save(fileName,trainingData)
 
-        lap = time.time() - prevTime
-        #print(lap)
+            lap = time.time() - prevTime
+            #print(lap)
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows() 
-            break
-    
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows() 
+                break
+
+main(fileName)
